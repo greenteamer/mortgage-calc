06f0f66ad47a2b3866b5e8700d678e295d78eec4
Aleksandr Korovkin <teamer777@gmail.com>
2022-11-21 06:27:16 +0400

    split to files

---
+++ mortgage.py
@@ -0,0 +1,55 @@
+from dateutil.relativedelta import relativedelta
+from tabulate import tabulate
+
+
+def get_coefficient(issue_rate):
+    result = (1 + issue_rate / 100) ** (1 / 12)
+    return result
+
+
+class MortgageTable:
+    def __init__(self, amount, issue_rate, loan_term, issue_date):
+        self.table = []
+        self.amount = amount
+        self.issue_rate = issue_rate
+        self.loan_term = loan_term
+        self.issue_date = issue_date
+        self.coefficient = get_coefficient(issue_rate)
+        self.month_amount = self.get_month_amount()
+        self.calc()
+
+    def __repr__(self) -> str:
+        headers = ["date", "payment", "month %", "main", "rest"]
+        return tabulate(self.table, headers, tablefmt="presto")
+
+    def calc(self):
+        rest_amount = self.amount
+        for m in range(self.loan_term):
+            date = self.issue_date + relativedelta(months=+m)
+            month_percent_amount = self.get_month_percent_by_amount(rest_amount)
+            month_main_amount = self.month_amount - month_percent_amount
+            rest_amount -= month_main_amount
+            self.table.append(
+                [
+                    date.strftime("%b %d %Y"),
+                    round(self.month_amount, 2),
+                    round(month_percent_amount, 2),
+                    round(month_main_amount, 2),
+                    round(rest_amount, 2),
+                ]
+            )
+
+    def get_month_amount(self):
+        month_issue_rate = self.coefficient - 1
+        result = (
+            self.amount
+            * pow(self.coefficient, self.loan_term)
+            * month_issue_rate
+            / (pow(self.coefficient, self.loan_term) - 1)
+        )
+        return result
+
+    def get_month_percent_by_amount(self, amount):
+        month_issue_rate = self.coefficient - 1
+        result = month_issue_rate * amount
+        return result
